5dd35a1752554ef9645386e8d03153e69350c4cc
wenshiuan1 <wenshiuan1@gmail.com>
2016-07-21 10:13:38 +0800

    app.blade and task.blade combine bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,52 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
 
-<html lang=\"en\">
-    <head>
-        <title>Laravel 基本範例</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSS 及 JavaScript -->
-    </head>
+    <title>Laravel 入門基本範例</title>
 
-    <body>
-        <div class=\"container\">
-            <nav class=\"navbar navbar-default\">
-                <!-- Navbar 內容 -->
-            </nav>
+    <!-- 字型 -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+    <!-- 風格 -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+
+</head>
+
+<body id="app-layout">
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <!-- TODO: Navbar 內容 -->
+
+            <!-- 首頁連結 -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Task List
+            </a>
+
         </div>
+    </div>
+</nav>
 
-        @yield('content_1')
-    </body>
+@yield('content_1')
+
+<!-- JavaScripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
 </html>
--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -2,32 +2,46 @@
 
 @section('content_1')
 
-    <!-- Bootstrap 樣板... -->
+    <!-- TODO: Bootstrap 樣板... -->
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
 
-    <div class="panel-body">
-
-        <!-- 新增任務用的表單 -->
-        <form action="{{ url("task") }}" method="POST" class="form-horizontal">
-        {{ csrf_field() }}
-
-        <!-- 任務名稱 -->
-            <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                <div class="panel-heading">
+                    New Task
                 </div>
-            </div>
 
-            <!-- 增加任務按鈕-->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i>增加任務</button>
+                <div class="panel-body">
+                    <!-- TODO: 顯示驗證錯誤 -->
+
+                    <!-- 新任務的表單 -->
+                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                    {{ csrf_field() }}
+
+                    <!-- TODO: 任務名稱 -->
+                        <div class="form-group">
+                            <label for="task" class="col-sm-3 control-label">Task</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- TODO: 增加任務按鈕-->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i>增加任務
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+
             </div>
-        </form>
+        </div>
     </div>
+
 
     <!-- 目前任務 -->
 @endsection
